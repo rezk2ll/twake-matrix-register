ab51ceb37e1949c32be604bf18f412c463c82951
Khaled FERJANI <kferjani@linagora.com>
2023-07-12 15:21:46 +0100

    added page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,7 @@
 	<meta name="keywords" content="Sveltekit, Matrix, Linagora, Twake" />
 	<meta name="author" content="Linagora" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Sign up on Twake</title>
 </svelte:head>
 
 <div class="min-h-screen w-full overflow-hidden bg-gray-900 flex flex-col">
