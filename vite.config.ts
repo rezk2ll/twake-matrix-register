d986c6b2b36a69d68d4aa876b9c7ddd41614a87a
Khaled FERJANI <kferjani@linagora.com>
2023-07-13 12:13:34 +0100

    changed default ports

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		port: 3000
+	},
+	preview: {
+		port: 3000
 	}
 });
