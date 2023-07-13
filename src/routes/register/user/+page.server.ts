031829125f4aefaeacf06a7e071383c9583b1bd7
Khaled FERJANI <kferjani@linagora.com>
2023-07-13 14:48:04 +0100

    added signup form action

--- src/routes/register/user/+page.server.ts
+++ src/routes/register/user/+page.server.ts
@@ -1,5 +1,6 @@
-import { redirect } from '@sveltejs/kit';
+import { redirect, type Actions, error, fail } from '@sveltejs/kit';
 import type { PageServerLoad } from '../$types';
+import { checkNickNameAvailability, signup } from '$lib/services/user';
 
 export const load: PageServerLoad = async ({ parent }) => {
 	const { session } = await parent();
@@ -10,5 +11,38 @@ export const load: PageServerLoad = async ({ parent }) => {
 
 	if (!session.verified) {
 		throw redirect(302, '/register/otp');
+	}
+};
+
+export const actions: Actions = {
+	default: async ({ request, locals }) => {
+		try {
+			const data = await request.formData();
+			const nickname = data.get('nickname') as string;
+			const displayName = (data.get('displayName') as string) ?? '';
+			const recoveryEmail = (data.get('recovery_email') as string) ?? '';
+
+			const { phone, verified } = locals.session.data;
+
+			if (!phone || !verified) {
+				return error(500);
+			}
+
+			if (!nickname) {
+				return fail(400, { nickname, missing: true });
+			}
+
+			if ((await checkNickNameAvailability(nickname)) === false) {
+				return fail(400, { nickname, taken: true });
+			}
+
+			await signup(nickname, phone, displayName, recoveryEmail);
+
+			return {
+				sucess: true
+			};
+		} catch (err) {
+			return error(500);
+		}
 	}
 };
