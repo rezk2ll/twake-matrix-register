c1e9da51a611805ac8853aec865b4bd68e53e7a7
Khaled FERJANI <kferjani@linagora.com>
2023-07-18 16:26:10 +0100

    init LDAP client

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,8 @@
 import type { PageServerLoad } from './$types';
+import Client from '$lib/services/ldap/client';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	await locals.session.destroy();
+
+	await Client.getClient();
 };
