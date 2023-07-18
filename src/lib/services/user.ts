bd3232280d609308dcaa2c0be7e130bcdd6de90c
Khaled FERJANI <kferjani@linagora.com>
2023-07-18 16:25:28 +0100

    added user service

--- src/lib/services/user.ts
+++ src/lib/services/user.ts
@@ -1,3 +1,6 @@
+import type { User } from '../../types';
+import ldapClient from './ldap/client';
+
 /**
  * Checks if a nickname is available.
  *
@@ -5,23 +8,76 @@
  * @returns {Promise<boolean>} - true if the nickname is available, false otherwise
  */
 export const checkNickNameAvailability = async (nickName: string): Promise<boolean> => {
-	return false;
+	try {
+		const existingUser = await ldapClient.find('cn', nickName, ['cn']);
+
+		if (existingUser.length === 0) {
+			return true;
+		}
+
+		return false;
+	} catch (error) {
+		console.log(error);
+
+		return false;
+	}
 };
 
 /**
- * Signs up a user to TOM.
+ * Checks if a phone is available.
  *
- * @param {string} nickName - the nickname of the user. 
- * @param {string} phone - the phone of the user.
+ * @param {string} phone - the phone to check for.
+ * @returns {Promise<boolean>} - true if the phone is available, false otherwise.
+ */
+export const checkPhoneAvailability = async (phone: string): Promise<boolean> => {
+	try {
+		const existingUser = await ldapClient.find('mobile', phone, ['cn']);
+
+		if (existingUser.length === 0) {
+			return true;
+		}
+
+		return false;
+	} catch (error) {
+		console.log(error);
+
+		return false;
+	}
+};
+
+/**
+ * Signs up a user
+ *
+ * @param {string} cn - the nickname of the user.
+ * @param {string} mobile - the phone of the user.
  * @param {string} displayName - the display name of the user.
- * @param {string} recoveryEmail - the email to send the recovery email to. 
- * @returns 
+ * @param {string} mail - the email to send the recovery email to.
  */
 export const signup = async (
-	nickName: string,
-	phone: string,
+	cn: string,
+	mobile: string,
 	displayName?: string,
-	recoveryEmail?: string
+	mail?: string
 ): Promise<void> => {
-	return;
+	try {
+		const entry: User = {
+			uid: cn,
+			cn,
+			sn: cn,
+			mobile,
+			objectclass: 'inetOrgPerson'
+		};
+
+		if (mail) {
+			entry.mail = mail;
+		}
+
+		if (displayName) {
+			entry.displayName = displayName;
+		}
+
+		await ldapClient.insert<User>(`cn=${cn},ou=users`, entry);
+	} catch (error) {
+		console.log(error);
+	}
 };
