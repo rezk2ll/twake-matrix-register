6f47123cf253d392bf15880bcce0c5bed9a69aaf
Khaled FERJANI <kferjani@linagora.com>
2023-07-20 14:12:10 +0100

    updated layout

--- src/routes/register/+layout.svelte
+++ src/routes/register/+layout.svelte
@@ -2,7 +2,7 @@
 	class="flex space-x-4 w-full min-w-screen min-h-screen bg-gray-900 items-center justify-center"
 >
 	<div
-		class="bg-gray-800 text-gray-500 rounded-xl shadow-xl px-2 py-16 md:px-16 w-full md:w-2/3 lg:w-1/3"
+		class="bg-gray-800 text-gray-500 rounded-xl shadow-xl px-2 py-16 md:px-16 w-full md:w-2/3 lg:w-2/5"
 	>
 		<slot />
 	</div>
