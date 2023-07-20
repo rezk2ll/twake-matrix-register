e912d9c5f5a144289dfbf9d80ee7a7673df35c49
Khaled FERJANI <kferjani@linagora.com>
2023-07-20 14:11:08 +0100

    added password checker

---
+++ src/lib/utils/password.ts
@@ -0,0 +1,14 @@
+/**
+ * Validates the complexity of a password
+ *
+ * a valid password should be:
+ *  - minimum 8 characters long
+ *  - contain at least one number
+ *  - contain at least one special character (!@#$%^&*)
+ *  - contain at least one lowercase letter
+ *
+ * @param {string} password - the password to validate  
+ * @returns {boolean} - whether the password is valid or not
+ */
+export const validatePassword = (password: string): boolean =>
+	/^(?=.*\d)(?=.*[!@#$%^&+*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/g.test(password);
