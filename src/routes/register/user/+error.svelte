a905a1ff37c383654f6f534701181b8702992cdf
Khaled FERJANI <kferjani@linagora.com>
2023-07-20 14:11:50 +0100

    updated error page

--- src/routes/register/user/+error.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Error from '$lib/components/dispaly/Error.svelte';
-</script>
-
-<div
-	class="flex space-x-4 w-full min-w-screen min-h-screen bg-gray-900 items-center justify-center px-5 py-5"
->
-	<Error />
-</div>
