228fe09ac8ab6a2e8a8bc8e581cd24e4031c80b1
Khaled FERJANI <kferjani@linagora.com>
2023-07-21 14:48:12 +0100

    customize session cookies

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,5 +2,7 @@ import { env } from '$env/dynamic/private';
 import { handleSession } from 'svelte-kit-cookie-session';
 
 export const handle = handleSession({
-	secret: env.SECRET
+	secret: env.SECRET,
+	key: 'twake.session',
+	expires: 1
 });
