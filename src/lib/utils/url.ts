500fa5cd63b4d38f0d2401ea4fd5347007d706ce
Khaled FERJANI <kferjani@linagora.com>
2023-10-30 09:13:23 +0100

    added domain extractor helper

--- src/lib/utils/url.ts
+++ src/lib/utils/url.ts
@@ -5,3 +5,19 @@
  * @returns {string} - the url with a trailing slash if it doesn't have one
  */
 export const getUrl = (url: string): string => (url.endsWith('/') ? url : `${url}/`);
+
+/**
+ * Extracts the domain name from a subdomain string.
+ *
+ * @param {string} subdomain - The input subdomain string.
+ * @returns {string} The extracted domain name.
+ */
+export const extractMainDomain = (subdomain: string): string => {
+	const match = subdomain.match(/[^.]+.[^.]+$/);
+
+	if (match) {
+		return match[0];
+	}
+
+	return subdomain;
+};
