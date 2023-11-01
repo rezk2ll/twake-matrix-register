af97da72956acec40705c218c06d2eeb1d9fc72c
Khaled FERJANI <kferjani@linagora.com>
2023-11-01 17:07:36 +0100

    added url helper

--- src/lib/utils/url.ts
+++ src/lib/utils/url.ts
@@ -1,3 +1,8 @@
+import { browser } from '$app/environment';
+import type { ApplicationType } from '../../types';
+import { getApplicationDeepLink, getApplicationStoreUrl } from './device';
+import base64url from 'base64url';
+
 /**
  * formats a url by adding a trailing slash if it doesn't have one
  *
@@ -20,4 +25,31 @@ export const extractMainDomain = (subdomain: string): string => {
 	}
 
 	return subdomain;
+};
+
+/**
+ * Opens the redirect deep link.
+ * 
+ * @param {string} redirect - The redirect url.
+ * @param {ApplicationType} app - The application to open.
+ */
+export const openAppDeepLink = (redirect: string, app: ApplicationType): void => {
+	if (!browser) return;
+
+	const url = new URL(redirect);
+
+	const appDeepLink = getApplicationDeepLink(app);
+	const appStoreUrl = getApplicationStoreUrl(app);
+
+	if (appDeepLink) {
+		url.searchParams.append('open_app', base64url(appDeepLink));
+	}
+
+	if (appStoreUrl) {
+		url.searchParams.append(appStoreUrl.type, base64url(appStoreUrl.url));
+	}
+
+	console.log(url.toString());
+
+	window.location.href = url.toString();
 };
