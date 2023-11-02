03491d8b8145f4ec4160a1c5cff3e912952c19d6
Khaled FERJANI <kferjani@linagora.com>
2023-11-02 11:13:27 +0100

    added device helpers

--- src/lib/utils/device.ts
+++ src/lib/utils/device.ts
@@ -1,11 +1,5 @@
 import { browser } from '$app/environment';
-import {
-	TMAIL_APPSTORE_URL,
-	TMAIL_PLAYSTORE_URL,
-	TWAKE_APPSTORE_URL,
-	TWAKE_PLAYSTORE_URL
-} from '$env/static/private';
-import type { ApplicationType, IApplicationStorePayload, PlatformType } from '../../types';
+import type { PlatformType } from '../../types';
 
 /**
  * Detects whether the user is on a mobile device or not.
@@ -29,64 +23,4 @@ export const getPlatform = (): PlatformType => {
 	if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) return 'ios';
 
 	return 'web';
-};
-
-/**
- * Get the store url for an application.
- *
- * @return {string | undefined}
- */
-export const getApplicationStoreUrl = (
-	app: ApplicationType
-): IApplicationStorePayload | undefined => {
-	if (isMobile()) {
-		const platform = getPlatform();
-
-		if (platform === 'android' && app === 'tmail') {
-			return {
-				type: 'play_store_url',
-				url: TMAIL_PLAYSTORE_URL
-			};
-		}
-
-		if (platform === 'android' && app === 'twake') {
-			return {
-				type: 'play_store_url',
-				url: TWAKE_PLAYSTORE_URL
-			};
-		}
-
-		if (platform === 'ios' && app === 'tmail') {
-			return {
-				type: 'app_store_url',
-				url: TMAIL_APPSTORE_URL
-			};
-		}
-
-		if (platform === 'ios' && app === 'twake') {
-			return {
-				type: 'app_store_url',
-				url: TWAKE_APPSTORE_URL
-			};
-		}
-	}
-};
-
-/**
- * Get the deep link for an application.
- *
- * @return {string | undefined}
- */
-export const getApplicationDeepLink = (app: ApplicationType): string | undefined => {
-	if (app === 'tdrive') {
-		return 'twake.drive://';
-	}
-
-	if (app === 'tmail') {
-		return 'teammail.mobile://';
-	}
-
-	if (app === 'twake') {
-		return 'twake.chat://';
-	}
 };
