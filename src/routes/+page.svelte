1a0c2893eddb5e7ba2576089d12a6d7249154ef9
Khaled FERJANI <kferjani@linagora.com>
2023-11-22 14:06:03 +0100

    Fixed main page responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,9 @@
 	$: formStore.set(form);
 </script>
 
-<div class="flex space-x-4 w-full min-h-screen md:px-10 2xl:px-52 py-5 pt-10">
+<div class="flex space-x-4 w-full min-h-screen md:px-10 2xl:px-52 md:py-5 pt-10">
 	<div class="w-full overflow-hidden">
-		<div class="flex flex-col md:flex-row w-full">
+		<div class="flex flex-col lg:flex-row w-full">
 			<Hero />
 			<AccessSelect />
 		</div>
