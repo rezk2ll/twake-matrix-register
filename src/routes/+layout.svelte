669224872d92e765f02f40285a42b45971949bcc
Khaled FERJANI <kferjani@linagora.com>
2023-11-22 14:05:38 +0100

    Fixed Inter font not being available in some devices

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,19 +8,20 @@
 	<meta name="keywords" content="Sveltekit, Matrix, Linagora, Twake" />
 	<meta name="author" content="Linagora" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
 	<title>Sign up on Twake</title>
 </svelte:head>
 
-<div class="min-h-screen w-full overflow-hidden gradiant flex flex-col">
+<div class="min-h-screen w-full overflow-hidden gradiant flex flex-col font-[Inter]">
 	<div class="bg-layout bg-no-repeat w-full">
 		<slot />
-    <div class="h-36 fixed bottom-0 w-full gradiant -z-30"></div>
+		<div class="h-36 fixed bottom-0 w-full gradiant -z-30" />
 	</div>
 </div>
 
 <style>
 	.gradiant {
-		background:linear-gradient(
+		background: linear-gradient(
 				105.88deg,
 				rgba(248, 169, 52, 0.2) 0%,
 				rgba(203, 40, 206, 0.2) 43.6%,
