b96274ef56aacb8e0b41469a689533e9d4a0e47e
Khaled FERJANI <kferjani@linagora.com>
2023-11-22 14:02:37 +0100

    added disabled state to submit button

--- src/lib/components/button/SubmitButton.svelte
+++ src/lib/components/button/SubmitButton.svelte
@@ -5,7 +5,9 @@
 <button
 	type="submit"
 	{disabled}
-	class="flex items-center rounded-full justify-center w-full h-12 bg-blue-500  text-base font-medium leading-5 tracking-wide text-center text-white"
+	class="flex items-center rounded-full justify-center w-full h-12 {disabled
+		? 'bg-slate-200 text-slate-400'
+		: 'bg-blue-500 text-white'} text-base font-medium leading-5 tracking-wide text-center "
 >
 	<slot />
 </button>
