59382754d97c48f435a460a45f68859533708082
Khaled FERJANI <kferjani@linagora.com>
2023-11-28 13:27:17 +0100

    updated the register form

--- src/lib/components/forms/RegisterForm.svelte
+++ src/lib/components/forms/RegisterForm.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
+	import UsedPhone from './../otp/UsedPhone.svelte';
+	import InfoTooltip from './../dispaly/InfoTooltip.svelte';
 	import TextField from '../input/TextField.svelte';
 	import PasswordField from '../input/PasswordField.svelte';
 	import PhoneField from '../input/PhoneField.svelte';
 	import SubmitButton from '../button/SubmitButton.svelte';
 	import VerifyPhoneModal from '../otp/VerifyPhoneModal.svelte';
-	import { form, phone as phoneStore } from '../../../store';
+	import { form, phone as phoneStore, verified } from '../../../store';
 	import { enhance } from '$app/forms';
 	import AvailableNicknames from '../user/AvailableNicknames.svelte';
 	import { createUserFormSchema, fullCreateUserFormSchema } from '$lib/schemas/zodSchema';
-	import { isNickNameTaken, suggestNickNames } from '$lib/utils/api';
+	import { isNickNameTaken, isPhoneTaken, suggestNickNames } from '$lib/utils/api';
 
 	let password = '';
 	let confirmPassword = '';
@@ -22,8 +24,12 @@
 	let loading = false;
 	let nickNameTaken = false;
 	let nickNamechecked = false;
+	let phoneTaken = false;
+	let loadingPhone = false;
+	let phoneChecked = false;
 
 	$: !!phone && phoneStore.set(phone);
+	$: invalidPhone = !createUserFormSchema.safeParse({ phone }).success || phoneTaken;
 
 	$: {
 		if ($form?.nickname_taken === true) nickNameTaken = true;
@@ -38,7 +44,10 @@
 			password,
 			confirmPassword,
 			accepted
-		}).success && nickNameTaken === false;
+		}).success ||
+		nickNameTaken ||
+		phoneTaken ||
+		!$verified;
 
 	$: validNickName = nickName.length > 0 && nickNamechecked && !nickNameTaken;
 
@@ -49,6 +58,7 @@
 		alternativeNicknames = [];
 		nickNameTaken = false;
 		nickNamechecked = true;
+		await new Promise((resolve) => setTimeout(resolve, 100));
 
 		const result = await isNickNameTaken(nickName);
 
@@ -61,16 +71,33 @@
 		nickNameTaken = result;
 	};
 
-	const invalidateNickNameCheck = () => (nickNamechecked = false);
+	const checkPhone = async () => {
+		if (!createUserFormSchema.safeParse({ phone }).success || phoneChecked) return;
+
+		loadingPhone = true;
+		phoneChecked = true;
+		phoneTaken = await isPhoneTaken(phone);
+
+		loadingPhone = false;
+	};
+
+	const invalidateNickNameCheck = () => {
+		nickNamechecked = false;
+		nickNameTaken = false;
+	};
+	const invalidatePhoneCheck = () => {
+		phoneChecked = false;
+		phoneTaken = false;
+	};
 </script>
 
 <form
 	use:enhance
 	action="?/register"
 	method="POST"
-	class="flex flex-col space-y-4 px-4 py-3 xl:px-3 lg:space-y-5 text-xs font-medium leading-4 tracking-wide text-left font-[Inter] w-full"
+	class="flex flex-col space-y-4 px-4 py-3 xl:px-3 lg:space-y-5 font-[Inter] w-full"
 >
-	<div class="flex space-x-5 w-full">
+	<div class="flex flex-col lg:flex-row lg:space-x-5 space-y-4 lg:space-y-0 w-full">
 		<TextField
 			name="firstname"
 			placeholder="First Name"
@@ -88,29 +115,35 @@
 			feedback={false}
 		/>
 	</div>
-	<TextField
-		name="nickname"
-		placeholder="Last Name"
-		label="Username"
-		bind:value={nickName}
-		isInValid={!createUserFormSchema.safeParse({ nickName }).success || nickNameTaken}
-		onBlur={checkNickName}
-		feedback={validNickName}
-		onInput={invalidateNickNameCheck}
-		{loading}
-	/>
-	{#if nickNameTaken === true}
-		<AvailableNicknames
+	<div class="w-full">
+		<TextField
+			name="nickname"
+			placeholder="Last Name"
+			label="Username"
 			bind:value={nickName}
-			nickNames={alternativeNicknames}
-			bind:show={nickNameTaken}
-			bind:checked={nickNamechecked}
+			isInValid={!createUserFormSchema.safeParse({ nickName }).success || nickNameTaken}
+			onBlur={checkNickName}
+			feedback={validNickName}
+			onInput={invalidateNickNameCheck}
+			{loading}
+			suffix="@twake.app"
+			info={true}
+			infoTitle="Matrix ID/Email"
+			infoDescription="Username you enter will be used to form you Matrix ID as well as your Twake mail address."
 		/>
-	{:else if nickName.length && !createUserFormSchema.safeParse({ nickName }).success}
-		<span class="text-xs font-medium leading-4 tracking-wide text-left text-red-500 px-5"
-			>invalid Username.
-		</span>
-	{/if}
+		{#if nickNameTaken === true}
+			<AvailableNicknames
+				bind:value={nickName}
+				nickNames={alternativeNicknames}
+				bind:show={nickNameTaken}
+				bind:checked={nickNamechecked}
+			/>
+		{:else if nickName.length && !createUserFormSchema.safeParse({ nickName }).success}
+			<span class="text-xs font-medium leading-4 tracking-wide text-left text-red-500 px-5"
+				>invalid Username.
+			</span>
+		{/if}
+	</div>
 
 	<PasswordField
 		name="password"
@@ -132,17 +165,28 @@
 		bind:value={confirmPassword}
 		isInvalid={!fullCreateUserFormSchema.safeParse({ password, confirmPassword }).success}
 	/>
-
-	<PhoneField bind:value={phone} isInValid={!createUserFormSchema.safeParse({ phone }).success}>
-		<VerifyPhoneModal bind:phone />
-	</PhoneField>
-	{#if $form?.invalid_phone}
-		<span class="text-xs font-medium leading-4 tracking-wide text-left text-red-500 px-5"
-			>invalid phone number
-		</span>
-	{/if}
-
-	<div class="flex flex-col items-center justify-center space-y-5">
+	<div class="max-w-full">
+		<PhoneField
+			bind:value={phone}
+			isInValid={invalidPhone}
+			onBlur={checkPhone}
+			onInput={invalidatePhoneCheck}
+			loading={loadingPhone}
+			checked={phoneChecked}
+		>
+			<VerifyPhoneModal bind:phone />
+		</PhoneField>
+		{#if $form?.invalid_phone}
+			<span class="text-xs font-medium leading-4 tracking-wide text-left text-red-500 px-5"
+				>invalid phone number
+			</span>
+		{:else if phoneTaken}
+			<div class="relative w-full">
+				<UsedPhone />
+			</div>
+		{/if}
+	</div>
+	<div class="flex flex-col items-center justify-center pt-8">
 		<SubmitButton {disabled}>Sign up</SubmitButton>
 	</div>
 	<div class="flex items-start space-x-5 xl:-mx-5">
