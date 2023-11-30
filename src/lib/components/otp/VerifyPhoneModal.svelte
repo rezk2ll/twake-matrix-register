2d4b5ffc0b7b81449ff8b6f4de1bd34b59b4a3b2
Khaled FERJANI <kferjani@linagora.com>
2023-11-30 14:07:32 +0100

    updated the verify phone dialog

--- src/lib/components/otp/VerifyPhoneModal.svelte
+++ src/lib/components/otp/VerifyPhoneModal.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import PrimaryButton from './../button/PrimaryButton.svelte';
 	import OutlineButton from './../button/OutlineButton.svelte';
 	import SubmitButton from '../button/SubmitButton.svelte';
 	import TextField from '../input/TextField.svelte';
@@ -50,7 +51,7 @@
 	}
 </script>
 
-<span class="absolute inset-y-0 right-0 flex items-center px-2">
+<span class="hidden absolute inset-y-0 right-0 lg:flex items-center px-2">
 	<button
 		type="button"
 		class="p-1 focus:outline-none focus:shadow-outline px-5 rounded-3xl bg-indigo-50 text-sm text-blue-500 font-medium leading-5 h-11 items-center"
@@ -59,6 +60,9 @@
 		Verify
 	</button>
 </span>
+<div class="lg:hidden pt-3 ">
+	<PrimaryButton handler={openVerificationModal}>Verify phone number</PrimaryButton>
+</div>
 <div
 	class="fixed w-full h-screen inset-0 z-50 overflow-hidden flex justify-center items-center {open ===
 	false
@@ -67,110 +71,108 @@
 	style="background: rgba(0,0,0,.6);"
 >
 	<div
-		class=" bg-white h-full lg:h-auto w-full md:max-w-md mx-auto rounded-2xl shadow-xl z-50 overflow-y-auto"
+		class="p-4 lg:px-[44px] bg-white h-full lg:h-auto w-full lg:max-w-md mx-auto lg:rounded-2xl shadow-xl z-50 overflow-y-auto"
 	>
-		<div class="text-left">
-			<div class="flex justify-between items-center">
-				<div class="hidden lg:block" />
+		<div class="flex justify-between items-center">
+			<div class="hidden lg:block" />
+			<button
+				class="modal-close hidden lg:block cursor-pointer z-50 -mr-8"
+				type="button"
+				on:click={() => (open = false)}
+			>
+				<svg
+					width="56"
+					height="56"
+					viewBox="0 0 56 56"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<g clip-path="url(#clip0_14295_114002)">
+						<path
+							fill-rule="evenodd"
+							clip-rule="evenodd"
+							d="M28 16.125C21.4331 16.125 16.125 21.4331 16.125 28C16.125 34.5669 21.4331 39.875 28 39.875C34.5669 39.875 39.875 34.5669 39.875 28C39.875 21.4331 34.5669 16.125 28 16.125ZM28 37.5C22.7631 37.5 18.5 33.2369 18.5 28C18.5 22.7631 22.7631 18.5 28 18.5C33.2369 18.5 37.5 22.7631 37.5 28C37.5 33.2369 33.2369 37.5 28 37.5ZM28 26.3256L32.2631 22.0625L33.9375 23.7369L29.6744 28L33.9375 32.2631L32.2631 33.9375L28 29.6744L23.7369 33.9375L22.0625 32.2631L26.3256 28L22.0625 23.7369L23.7369 22.0625L28 26.3256Z"
+							fill="#1C1B1F"
+						/>
+					</g>
+					<defs>
+						<clipPath id="clip0_14295_114002">
+							<rect x="4.25" y="4.25" width="47.5" height="47.5" rx="23.75" fill="white" />
+						</clipPath>
+					</defs>
+				</svg>
+			</button>
+			<div class="lg:hidden" />
+		</div>
+		<form use:enhance action="?/sendOtp" method="POST" class="hidden" bind:this={sendOtpForm}>
+			<input type="text" name="phone" bind:value={phone} required />
+		</form>
+		<div class="flex flex-col space-y-2">
+			<div class="flex flex-row space-x-2 lg:items-center lg:justify-center">
 				<button
-					class="modal-close hidden lg:block cursor-pointer z-50"
+					class="lg:hidden modal-close cursor-pointer z-50"
 					type="button"
 					on:click={() => (open = false)}
 				>
-					<svg
-						width="56"
-						height="56"
-						viewBox="0 0 56 56"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<g clip-path="url(#clip0_14295_114002)">
-							<path
-								fill-rule="evenodd"
-								clip-rule="evenodd"
-								d="M28 16.125C21.4331 16.125 16.125 21.4331 16.125 28C16.125 34.5669 21.4331 39.875 28 39.875C34.5669 39.875 39.875 34.5669 39.875 28C39.875 21.4331 34.5669 16.125 28 16.125ZM28 37.5C22.7631 37.5 18.5 33.2369 18.5 28C18.5 22.7631 22.7631 18.5 28 18.5C33.2369 18.5 37.5 22.7631 37.5 28C37.5 33.2369 33.2369 37.5 28 37.5ZM28 26.3256L32.2631 22.0625L33.9375 23.7369L29.6744 28L33.9375 32.2631L32.2631 33.9375L28 29.6744L23.7369 33.9375L22.0625 32.2631L26.3256 28L22.0625 23.7369L23.7369 22.0625L28 26.3256Z"
-								fill="#1C1B1F"
-							/>
-						</g>
-						<defs>
-							<clipPath id="clip0_14295_114002">
-								<rect x="4.25" y="4.25" width="47.5" height="47.5" rx="23.75" fill="white" />
-							</clipPath>
-						</defs>
-					</svg>
+					<ArrowLeft />
 				</button>
-				<div class="lg:hidden" />
-			</div>
-			<form use:enhance action="?/sendOtp" method="POST" class="hidden" bind:this={sendOtpForm}>
-				<input type="text" name="phone" bind:value={phone} required />
-			</form>
-			<div class="flex flex-col space-y-2">
-				<div class="flex flex-row pl-10 space-x-2 pt-10">
-					<button
-						class="lg:hidden modal-close cursor-pointer z-50"
-						type="button"
-						on:click={() => (open = false)}
-					>
-						<ArrowLeft />
-					</button>
 
-					<h1 class="hidden lg:block text-2xl font-semibold leading-8 tracking-normal text-center">
-						Phone number confirmation
-					</h1>
-					<h1 class="lg:hidden text-2xl font-semibold leading-8 tracking-normal text-left">
-						Confirm your phone number
-					</h1>
-				</div>
-				{#if invalidPhone}
-					<span class="text-base font-medium leading-6 tracking-tight text-center text-red-400"
-						>this phone is already taken</span
-					>
-				{:else}
-					<span class="text-base font-medium leading-6 tracking-tight text-center text-gray-400"
-						>Enter 6 digit code we sent to:</span
-					>
-					<span class="text-base font-medium leading-6 tracking-tight text-center"
-						>{maskPhone(phone)}</span
-					>
-				{/if}
+				<h1 class="hidden lg:block text-2xl font-semibold leading-8 tracking-normal text-center">
+					Phone number confirmation
+				</h1>
+				<h1 class="lg:hidden text-2xl font-semibold leading-8 tracking-normal text-left">
+					Confirm your phone number
+				</h1>
 			</div>
-
-			{#if !invalidPhone}
-				<form action="?/checkOtp" use:enhance method="POST" class="px-10 pb-10">
-					<div class="my-5">
-						<TextField
-							label="Code"
-							placeholder="Code"
-							bind:value
-							name="password"
-							isInValid={incorrect}
-							feedback={false}
-						/>
-						{#if incorrect}
-							<span class="text-xs font-medium leading-4 tracking-tight text-left text-red-500 px-5"
-								>Entered code is incorrect. Try again.
-							</span>
-						{/if}
-						{#if timeout}
-							<span class="text-xs font-medium leading-4 tracking-tight text-left text-red-500 px-5"
-								>too many wrong attempts has been made. try again later.
-							</span>
-						{/if}
-					</div>
-					<div class="flex flex-col space-y-2 pt-2">
-						<SubmitButton disabled={inValid}>Confirm</SubmitButton>
-						<OutlineButton handler={handleSendOtp} disabled={resendCounter > 0 || timeout}>
-							{#if resendCounter === 0}
-								Send code
-							{:else}
-								Send code again in: 0:{resendCounter}
-							{/if}
-						</OutlineButton>
-					</div>
-				</form>
+			{#if invalidPhone}
+				<span class="text-base font-medium leading-6 tracking-tight text-center text-red-400"
+					>this phone is already taken</span
+				>
 			{:else}
-				<div class="px-10 pb-10" />
+				<span class="text-base font-medium leading-6 tracking-tight text-center text-gray-400"
+					>Enter 6 digit code we sent to:</span
+				>
+				<span class="text-base font-medium leading-6 tracking-tight text-center"
+					>{maskPhone(phone)}</span
+				>
 			{/if}
 		</div>
+
+		{#if !invalidPhone}
+			<form action="?/checkOtp" use:enhance method="POST" class="pb-10">
+				<div class="my-5">
+					<TextField
+						label="Code"
+						placeholder="Code"
+						bind:value
+						name="password"
+						isInValid={incorrect}
+						feedback={false}
+					/>
+					{#if incorrect}
+						<span class="text-xs font-medium leading-4 tracking-tight text-left text-red-500 px-5"
+							>Entered code is incorrect. Try again.
+						</span>
+					{/if}
+					{#if timeout}
+						<span class="text-xs font-medium leading-4 tracking-tight text-left text-red-500 px-5"
+							>too many wrong attempts has been made. try again later.
+						</span>
+					{/if}
+				</div>
+				<div class="flex flex-col space-y-2 pt-2">
+					<SubmitButton disabled={inValid}>Confirm</SubmitButton>
+					<OutlineButton handler={handleSendOtp} disabled={resendCounter > 0 || timeout}>
+						{#if resendCounter === 0}
+							Send code
+						{:else}
+							Send code again in: 0:{resendCounter}
+						{/if}
+					</OutlineButton>
+				</div>
+			</form>
+		{:else}
+			<div class="px-10 pb-10" />
+		{/if}
 	</div>
 </div>
