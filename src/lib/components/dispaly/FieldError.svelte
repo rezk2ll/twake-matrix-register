adee7b430269e3e7b39aa3584dbfe4feaae56908
Khaled FERJANI <kferjani@linagora.com>
2023-11-30 14:05:05 +0100

    added field error component

---
+++ src/lib/components/dispaly/FieldError.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let error: string;
+</script>
+
+<span
+	class="text-[#FF3347] text-xs not-italic font-medium leading-4 tracking-[0.5px] px-4 text-left"
+>
+	{error}
+</span>
