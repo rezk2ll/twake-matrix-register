5db4a0a3c5176f7b7d9e3bb6663cd908ecf79caf
Khaled FERJANI <kferjani@linagora.com>
2023-11-30 14:05:24 +0100

    added primary button component

---
+++ src/lib/components/button/PrimaryButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let disabled: boolean = false;
+	export let handler: () => void = () => {};
+</script>
+
+<button
+	type="button"
+	{disabled}
+  on:click={handler}
+	class="flex items-center rounded-full justify-center w-full h-12 {disabled
+		? 'bg-slate-200 text-slate-400'
+		: 'bg-blue-500 text-white'} text-base font-medium leading-5 tracking-wide text-center"
+>
+	<slot />
+</button>
