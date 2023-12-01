bfd2f81c6c56ee96e28d289c352d38e21d99ba6c
Khaled FERJANI <kferjani@linagora.com>
2023-12-01 16:11:10 +0100

    updated outline button

--- src/lib/components/button/OutlineButton.svelte
+++ src/lib/components/button/OutlineButton.svelte
@@ -9,9 +9,9 @@
 	type="button"
 	on:click={handler}
 	{disabled}
-	class="flex items-center rounded-full justify-center w-full h-12 border border-primary {disabled
+	class="flex items-center rounded-full justify-center w-full h-12 border  {disabled
 		? ' border-gray-300 text-gray-300'
-		: ''} text-base font-medium leading-5 tracking-wide text-center text-primary"
+		: 'text-primary border-primary'} text-base font-medium leading-5 tracking-wide text-center "
 >
 	<slot />
 </button>
