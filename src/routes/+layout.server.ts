8cec0eba52a0c21edb82dcedc32595077b56418b
Khaled FERJANI <kferjani@linagora.com>
2023-12-04 11:12:41 +0100

    remove i18n init from layout server load

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,16 +1,5 @@
-import { browser } from '$app/environment';
-import { getLocaleFromNavigator, locale, waitLocale } from 'svelte-i18n';
 import type { LayoutServerLoad } from './$types';
-import '$lib/i18n';
 
-export const load: LayoutServerLoad = async ({ locals }) => {
-	if (browser) {
-		locale.set(getLocaleFromNavigator());
-	}
-
-	await waitLocale();
-
-	return {
-		session: locals.session.data
-	};
-};
+export const load: LayoutServerLoad = async ({ locals }) => ({
+	session: locals.session.data
+});
