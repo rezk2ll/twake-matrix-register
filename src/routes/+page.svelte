dab52a9d5a0d7e5b56b7e196112a961f721225b8
Khaled FERJANI <kferjani@linagora.com>
2023-12-22 11:27:40 +0100

    switch to login tab when using a redirect url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-	import { form as formStore } from './../store';
+	import { activeTab, form as formStore } from './../store';
 	import AccessSelect from '$lib/components/landing/AccessSelect.svelte';
 	import Hero from '$lib/components/landing/Hero.svelte';
-	import type { ActionData } from './$types';
+	import type { ActionData, PageData } from './$types';
 
 	export let form: ActionData;
-
+	export let data: PageData;
 	$: formStore.set(form);
+	$: activeTab.set(data.isLogin ? 'login' : 'register');
 </script>
 
 <div class="flex w-full h-full md:py-5 lg:px-10 xl:px-[124px]">
-	<div class="flex flex-col lg:flex-row w-full overflow-hidden pt-9 ">
+	<div class="flex flex-col lg:flex-row w-full overflow-hidden pt-9">
 		<Hero />
 		<AccessSelect />
 	</div>
