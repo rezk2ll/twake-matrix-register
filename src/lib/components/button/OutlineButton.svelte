970ca0a783effdb32da14a379a58c2e836ebf184
Khaled FERJANI <kferjani@linagora.com>
2024-01-02 15:33:31 +0100

    #1227 Fixed send again in text color

--- src/lib/components/button/OutlineButton.svelte
+++ src/lib/components/button/OutlineButton.svelte
@@ -10,8 +10,9 @@
 	on:click={handler}
 	{disabled}
 	class="flex items-center rounded-full justify-center w-full h-12 border  {disabled
-		? ' border-gray-300 text-gray-300'
+		? ' border-disabled text-disabled-text text-opacity-[0.38]'
 		: 'text-primary border-primary'} text-base font-medium leading-5 tracking-wide text-center "
 >
 	<slot />
 </button>
+
