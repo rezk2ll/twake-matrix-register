e63ae756b4c0d00ea044a897cef436e2baebaa36
Khaled FERJANI <kferjani@linagora.com>
2024-01-02 10:49:18 +0100

    #1249 fixed login with sso letter spacing

--- src/lib/components/link/OutlineLink.svelte
+++ src/lib/components/link/OutlineLink.svelte
@@ -4,7 +4,7 @@
 
 <a
 	{href}
-	class="flex items-center rounded-full justify-center w-full h-12 border border-blue-500 text-base font-medium leading-5 tracking-wide text-center text-blue-500"
+	class="flex items-center rounded-full justify-center w-full h-12 border border-blue-500 text-base font-medium leading-5 tracking-[0.1px] text-center text-blue-500"
 >
 	<slot />
 </a>
